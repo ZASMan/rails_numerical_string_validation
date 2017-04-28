7bc09c522ccc1ca88753be8422bc4d8aa0c85da4
Zane <i.zane.am.robot@gmail.com>
2017-04-28 12:23:13 -0400

    Update example_model.rb

--- example_model.rb
+++ example_model.rb
@@ -1,11 +1,18 @@
-def string_only_numbers
-  if model_field.nil?
-    errors.add(:model_field, 'cannot be blank')
-  else
-    string_numbers = (0..9).to_a.join('').to_s.split('')
-    model_field_chars = model_field.split('')
-    model_field_chars.each do |num|
-      errors.add(:model_field, 'can only contain valid numbers (0-9).') unless string_numbers.include?(num)
+class ExampleModel < ActiveRecord::Base
+  validates :model_field, uniqueness: true, presence: true,
+  length: { minimum: 12, maximum: 12, message: 'must be 12 numbers' }
+  validate :string_only_contain_numbers    
+  
+  
+  def string_only_contain_numbers
+    if model_field.nil?
+      errors.add(:model_field, 'cannot be blank')
+    else
+      string_numbers = (0..9).to_a.join('').to_s.split('')
+      model_field_chars = model_field.split('')
+      model_field_chars.each do |num|
+        errors.add(:model_field, 'can only contain valid numbers (0-9).') unless string_numbers.include?(num)
+      end
     end
   end
 end
